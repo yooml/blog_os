25509bd17add69b1f70d2777655f4f5f8eb95f1b
yooml <947801604@qq.com>
2020-02-14 22:41:15 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+//fn main() {
+//}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
