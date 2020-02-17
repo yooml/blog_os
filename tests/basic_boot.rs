a028f26f8afb27939b066e0e24b5a4dbebdb8437
yooml <ymlll0508@gmail.com>
2020-02-17 20:35:21 +0800

    post-05

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -6,9 +6,10 @@
 
 use core::panic::PanicInfo;
 
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    init();
+    //init();
     test_main();
 
     loop {}
