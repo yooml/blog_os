41f54bc67311a9d94a9677dafc29df466fa38417
yooml <947801604@qq.com>
2020-03-08 20:47:58 +0800

    post-08结束，获取CR3中物理地址，和访问代码段所在的只读页

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -41,12 +41,29 @@ lazy_static! (
             .set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 );
 
 pub fn init_idt() {
     IDT.load();
+}
+
+use x86_64::structures::idt::PageFaultErrorCode;
+use crate::hlt_loop;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(
--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,19 @@ pub extern "C" fn _start() -> ! {
     // invoke a breakpoint exception
     x86_64::instructions::interrupts::int3(); // new
     */
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    let ptr = 0x2031b2 as *mut u32;
+
+// read from a code page
+    unsafe { let x = *ptr; }
+    println!("read worked");
+
+// write to a code page
+    unsafe { *ptr = 42; }
+    println!("write worked");
     #[cfg(test)]
         test_main();
     //panic!("Some panic message");
