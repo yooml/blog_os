10ff16c5d55d47006128640cc35d9d25b337e861
yooml <947801604@qq.com>
2020-03-09 21:52:41 +0800

    实现虚拟地址到物理地址的翻译

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,15 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
+
 
 use core::panic::PanicInfo;
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
 
 pub fn init() {
     gdt::init();
@@ -37,8 +44,9 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+//#[no_mangle]
+//pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,64 @@ mod vga_buffer;
 mod serial;
 
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
 
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use blog_os::memory::active_level_4_table;
+    use x86_64::VirtAddr;
+    use blog_os::memory::translate_addr;
+
+    println!("Hello World{}", "!");
+    blog_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    for (i, entry) in l4_table.iter().enumerate() {
+        use x86_64::structures::paging::PageTable;
+
+        if !entry.is_unused() {
+            println!("L4 Entry {}: {:?}", i, entry);
+            // get the physical address from the entry and convert it
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            // print non-empty entries of the level 3 table
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("  L3 Entry {}: {:?}", i, entry);
+                }
+            }
+        }
+    }
+
+    // as before
+    #[cfg(test)]
+        test_main();
+
+    println!("It did not crash!");
+    blog_os::hlt_loop();
+}
 
 /*#[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -35,8 +92,9 @@ fn panic(info: &PanicInfo) -> ! {
 //fn main() {
 //}
 
+/*
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
     blog_os::init();
 
     /*fn stack_overflow() {
@@ -77,4 +135,4 @@ pub extern "C" fn _start() -> ! {
     blog_os::hlt_loop();
 }
 
-
+*/
